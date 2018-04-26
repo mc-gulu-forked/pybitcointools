b9496de67ac6a92091e8e655a1a601053d68165e
Gu Lu (Surface) <gemfeeling@hotmail.com>
2018-04-26 16:59:57 +0800

    add error_code & error_text for `push_tx`

--- cryptos/explorers/bc_shared.py
+++ cryptos/explorers/bc_shared.py
@@ -3,6 +3,16 @@ import requests
 
 from cryptos.transaction import public_txhash
 
+ERR_OK = 0
+ERR_Unknown = 1
+ERR_FeeNotEnough = 2
+
+ErrText = {
+    ERR_OK:             "ok",
+    ERR_Unknown:        "unknown",
+    ERR_FeeNotEnough:   "fee not enough",
+}
+
 def request_push_tx(url, tx, coin_symbol):
     hash = public_txhash(tx)
     response = requests.post(url, {'tx': tx})
@@ -11,15 +21,25 @@ def request_push_tx(url, tx, coin_symbol):
                 'data': {
                     'txid': hash,
                     'network': coin_symbol
-                    }
+                    },
+                'detail': {
+                    'error_code': ERR_OK, 
+                    'error_text': ErrText[ERR_OK], 
+                    },
                 }
     else:
+        err = ERR_Unknown
+        if response.status_code == 500 and 'min relay fee not met' in response.text:
+            err = ERR_FeeNotEnough
+            
         return {'status': 'fail',
                 'data': {
                     'txid': hash,
                     'network': coin_symbol
                     },
                 'detail': {
+                    'error_code': err, 
+                    'error_text': ErrText[err], 
                     'status_code': response.status_code, 
                     'text': response.text,
                     'reason': response.reason,
